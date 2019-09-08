0644030ed14f47fbe6d571583424b0c5a2f1fc1a
chrissyalbert <chrissy_albert@yahoo.com>
2019-09-07 22:07:16 -0700

    Add artistsRouter post '/' route

--- api/artists.js
+++ api/artists.js
@@ -30,4 +30,37 @@ artistsRouter.get('/:artistId', (req, res, next) => {
   res.status(200).json({artist: req.artist});
 });
 
+artistsRouter.post('/', (req, res, next) => {
+  const name = req.body.artist.name;
+  const dateOfBirth = req.body.artist.dateOfBirth;
+  const biography = req.body.artist.biography;
+
+  if (!name || !dateOfBirth || !biography) {
+    return res.sendStatus(400);
+  }
+
+  const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+  const sql = 'INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)';
+  const values = {
+    $name: name,
+    $dateOfBirth: dateOfBirth,
+    $biography: biography,
+    $isCurrentlyEmployed: isCurrentlyEmployed
+  };
+
+  db.run(sql, values, function(error) {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Artist WHERE id = ${this.lastID}`, (error, artist) => {
+        if (error) {
+          next(error);
+        } else {
+          res.status(201).json({artist: artist});
+        } 
+      });
+    }
+  });
+});
+
 module.exports = artistsRouter;
--- server.js
+++ server.js
@@ -4,20 +4,14 @@ const cors = require('cors');
 const errorhandler = require('errorhandler');
 const morgan = require('morgan');
 const app = express();
+const PORT = process.env.PORT || 4000;
+const apiRouter = require('./api/api');
 
 app.use(bodyParser.json())
 app.use(cors());
-if (process.env.NODE_ENV === 'development') {
-  // only use in development
-  app.use(errorhandler());
-};
-app.use(morgan('tiny'));
-
-const apiRouter = require('./api/api');
+app.use(morgan('dev'));
 app.use('/api', apiRouter);
-
-
-const PORT = process.env.PORT || 4000;
+app.use(errorhandler());
 
 app.listen(PORT, function () {
   console.log('CORS-enabled web server listening on port 4000');
