ff97e368e58a78bb7d519b4f18b26bd0e0445fd3
chrissyalbert <chrissy_albert@yahoo.com>
2019-09-10 14:42:30 -0700

    add series get '/' route, add series param 'seriesId'

--- api/series.js
+++ api/series.js
@@ -4,4 +4,26 @@ const seriesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+seriesRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Series', (error, series) => {
+    if (error) {
+      next(error);
+    }
+    res.status(200).json({series: series});
+  })
+});
+
+seriesRouter.param('seriesId', (req, res, next, seriesId) => {
+  db.get('SELECT * FROM Series WHERE id = $seriesId', {$seriesId: seriesId}, (error, series) => {
+    if (error) {
+      next(error);
+    } else if (series) {
+      req.series = series;
+      next();
+    } else {
+      res.sendstatus(404);
+    }
+  });
+});
+
 module.exports = seriesRouter;
